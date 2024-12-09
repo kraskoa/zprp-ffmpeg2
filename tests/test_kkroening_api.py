562676adb02496fe6f1a2a4c55083a75da631289
kraskoa <adampkras@gmail.com>
2024-12-09 19:51:06 +0100

    Added tests from original repo, not passing

--- tests/test_kkroening_api.py
+++ tests/test_kkroening_api.py
@@ -1,5 +1,6 @@
 # test if the package structure matches
 import json
+import sys
 from pathlib import Path
 
 import pytest
@@ -128,3 +129,61 @@ def test_probe():
     # try to decode, by default it's json
     obj = json.loads(out)
     assert "streams" in obj
+
+def test_fluent_output():
+    ffmpeg.input('dummy.mp4').trim(start_frame=10, end_frame=20).output('dummy2.mp4')
+
+def test_fluent_equality():
+    base1 = ffmpeg.input('dummy1.mp4')
+    base2 = ffmpeg.input('dummy1.mp4')
+    base3 = ffmpeg.input('dummy2.mp4')
+    t1 = base1.trim(start_frame=10, end_frame=20)
+    t2 = base1.trim(start_frame=10, end_frame=20)
+    t3 = base1.trim(start_frame=10, end_frame=30)
+    t4 = base2.trim(start_frame=10, end_frame=20)
+    t5 = base3.trim(start_frame=10, end_frame=20)
+    print(t1, t2, t3, t4, t5)
+    assert t1 == t2
+    assert t1 != t3
+    assert t1 == t4
+    assert t1 != t5
+
+def test_escape_chars():
+    assert ffmpeg._utils.escape_chars('a:b', ':') == r'a\:b'
+    assert ffmpeg._utils.escape_chars('a\\:b', ':\\') == 'a\\\\\\:b'
+    assert (
+        ffmpeg._utils.escape_chars('a:b,c[d]e%{}f\'g\'h\\i', '\\\':,[]%')
+        == 'a\\:b\\,c\\[d\\]e\\%{}f\\\'g\\\'h\\\\i'
+    )
+    assert ffmpeg._utils.escape_chars(123, ':\\') == '123'
+
+def test_repeated_args():
+    out_file = ffmpeg.input('dummy.mp4').output(
+        'dummy2.mp4', streamid=['0:0x101', '1:0x102']
+    )
+    assert out_file.get_args() == [
+        '-i',
+        'dummy.mp4',
+        '-streamid',
+        '0:0x101',
+        '-streamid',
+        '1:0x102',
+        'dummy2.mp4',
+    ]
+
+def test__get_args__simple():
+    out_file = ffmpeg.input('dummy.mp4').output('dummy2.mp4')
+    assert out_file.get_args() == ['-i', 'dummy.mp4', 'dummy2.mp4']
+
+def test__output__bitrate():
+    args = (
+        ffmpeg.input('in')
+        .output('out', video_bitrate=1000, audio_bitrate=200)
+        .get_args()
+    )
+    assert args == ['-i', 'in', '-b:v', '1000', '-b:a', '200', 'out']
+
+@pytest.mark.parametrize('video_size', [(320, 240), '320x240'])
+def test__output__video_size(video_size):
+    args = ffmpeg.input('in').output('out', video_size=video_size).get_args()
+    assert args == ['-i', 'in', '-video_size', '320x240', 'out']
