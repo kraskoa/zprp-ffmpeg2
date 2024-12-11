85c4fd5d7d60070994b0e46de76c94c241613c41
chmielg <gregchmielewski@gmail.com>
2024-12-11 15:26:33 +0100

    Added input kwargs test

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -22,3 +22,27 @@ def test_concat_fade_duplicate():
         "[v2]",
         "output.mp4"
     ]
+
+
+def test_concat_fade_duplicate_with_time_parameter5():
+    stream = ffmpeg.input("input.mp4", t=5)
+    stream = ffmpeg.fade(stream, type="in", start_frame=0, nb_frames=30)
+    stream = ffmpeg.concat((stream, stream))
+    stream = ffmpeg.output(stream, "output.mp4")
+    assert get_args(stream) == [
+        "-t",
+        "5",
+        "-i",
+        "input.mp4",
+        "-t",
+        "5",
+        "-i",
+        "input.mp4",
+        "-filter_complex",
+        '"[0:v]fade=type=in:nb_frames=30[v0];',
+        "[1:v]fade=type=in:nb_frames=30[v1];",
+        '[v0][v1]concat[v2]"',
+        "-map",
+        "[v2]",
+        "output.mp4"
+    ]
