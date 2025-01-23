3379541c71a283b2e5004ba36b4d68d1497ad330
kraskoa <adampkras@gmail.com>
2025-01-23 16:25:02 +0100

    Work on fixing mypy errors

--- src/zprp_ffmpeg2/view.py
+++ src/zprp_ffmpeg2/view.py
@@ -1,15 +1,9 @@
 import dataclasses
 from enum import Enum
 from re import sub
-from typing import List
-from typing import Optional
+from typing import List, Optional, Union
 
-from .filter_graph import AnyNode
-from .filter_graph import Filter
-from .filter_graph import MergeOutputFilter
-from .filter_graph import SinkFilter
-from .filter_graph import SourceFilter
-from .filter_graph import Stream
+from .filter_graph import AnyNode, Filter, MergeOutputFilter, SinkFilter, SourceFilter, Stream
 
 
 class NodeColors(Enum):
@@ -27,65 +21,69 @@ class PrepNode:
     def create_path_for_next(self) -> str:
         if "|" in self.path:
             return self.name
-        sep = ""
-        if self.path:
-            sep = ";"
+        sep = ";" if self.path else ""
         return f"{self.path}{sep}{self.name}"
 
-    def prev_node(self) -> List[str] | None:
+    def prev_node(self) -> Optional[List[str]]:
         if not self.path:
             return None
         return [path.split(";")[-1] for path in self.path.split("|")]
 
     @property
-    def id(self):
+    def id(self) -> int:
         if (string := sub(r"\w+\((\d+)\)", r"\1", self.name)) == self.name or "." in self.name:
             return 1
-        else:
-            return int(string)
+        return int(string)
 
     @property
-    def command(self):
-        if "." in self.name:
-            return self.name
-        else:
-            return sub(r"(\w+)\(\d+\)", r"\1", self.name)
+    def command(self) -> str:
+        return self.name if "." in self.name else sub(r"(\w+)\(\d+\)", r"\1", self.name)
 
 
-def create_graph_connections(parent_node: AnyNode | Stream, previous: List[PrepNode]) -> None:
-    """Creates list of PrepNode objects that contain stream data to later
-    transform it to a graph
-    """
-    new_connections = []
-    nodes = None
+def create_graph_connections(parent_node: Union[AnyNode, Stream], previous: List[PrepNode]) -> None:
+    """Creates list of PrepNode objects that contain stream data to later transform it into a graph."""
+    new_connections: List[PrepNode] = []
+
+    # Ensuring correct node access
     if isinstance(parent_node, Filter):
-        nodes = parent_node._in
+        nodes: List[Union[SourceFilter, SinkFilter, Filter]] = parent_node._in
+    elif isinstance(parent_node, Stream):
+        nodes: List[Union[SourceFilter, SinkFilter, Filter]] = parent_node._nodes  # Ensure Stream has _nodes
     else:
-        nodes = parent_node._nodes
+        return  # Avoid processing unknown types
+
     for node in nodes:
         if isinstance(node, SourceFilter):
             new_connections.append(PrepNode(node.in_path.split("/")[-1], NodeColors.INPUT, ""))
         elif isinstance(node, SinkFilter):
-            new_connections.append(PrepNode(node.out_path.split("/")[-1], NodeColors.OUTPUT, new_connections[-1].create_path_for_next()))
+            if new_connections:
+                new_connections.append(
+                    PrepNode(node.out_path.split("/")[-1], NodeColors.OUTPUT, new_connections[-1].create_path_for_next())
+                )
         elif isinstance(node, Filter):
             path = ""
             if not new_connections:
                 create_graph_connections(node, previous)
                 paths = []
                 for stream in node._in:
-                    last_node = stream._nodes[-1]
-                    parent_prep_node = None
+                    last_node = stream._nodes[-1] if hasattr(stream, "_nodes") else None
                     if isinstance(last_node, Filter):
                         parent_prep_node = next(
-                            prep_node for prep_node in previous if ((prep_node.command, prep_node.id) == (last_node.command, last_node._id))
+                            prep_node for prep_node in previous
+                            if (prep_node.command, prep_node.id) == (last_node.command, getattr(last_node, "_id", -1))
+                        )
+                    elif isinstance(last_node, SourceFilter):
+                        parent_prep_node = next(
+                            prep_node for prep_node in previous if prep_node.name == last_node.in_path
                         )
                     else:
-                        parent_prep_node = next(prep_node for prep_node in previous if prep_node.name == last_node.in_path)
+                        continue
                     paths.append(parent_prep_node.create_path_for_next())
                 path = "|".join(paths)
             else:
                 path = new_connections[-1].create_path_for_next()
-            suffix = "" if node._id < 2 else f"({node._id})"
+
+            suffix = f"({getattr(node, '_id', 1)})" if getattr(node, "_id", 1) >= 2 else ""
             new_connections.append(PrepNode(f"{node.command}{suffix}", NodeColors.FILTER, path))
         elif isinstance(node, MergeOutputFilter):
             for stream in node.streams:
@@ -93,24 +91,25 @@ def create_graph_connections(parent_node: AnyNode | Stream, previous: List[PrepN
             return
         elif isinstance(node, Stream):
             create_graph_connections(node, previous)
+
     previous.extend(new_connections)
 
 
 def view(graph: Stream, filename: Optional[str] = None) -> None:
-    "Creates a graph of filters"
+    """Creates a graph of filters."""
 
     import networkx as nx  # type: ignore
     from matplotlib import pyplot as plt  # type: ignore
 
     G = nx.DiGraph()
 
-    graph_connection = []
+    graph_connection: List[PrepNode] = []
     create_graph_connections(graph, graph_connection)
-    graph_connection = list(dict.fromkeys(graph_connection))
+    graph_connection = list(dict.fromkeys(graph_connection))  # Remove duplicates while keeping order
 
     # Adding nodes
     for pre_node in graph_connection:
-        G.add_node(pre_node.name, color=pre_node.color)
+        G.add_node(pre_node.name, color=pre_node.color.value)
 
     # Adding edges
     for pre_node in graph_connection:
